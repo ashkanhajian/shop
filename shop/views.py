0b9ed9530a0d194a0feb3075c0174c279be69205
nell2008 <ashkan.spg@gmail.com>
2024-06-25 17:21:08 -0500

    1

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
 
 # Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    }
+    return render(request, 'shop/list.html')
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, slug=slug, id=id)
+    return render(request, 'shop/detail.html', {'product': product})
