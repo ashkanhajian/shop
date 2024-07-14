2815bdf5fcaa564e6c676b8b0aefba2cbb2232bb
nell2008 <ashkan.spg@gmail.com>
2024-07-14 17:55:16 -0500

    1

--- account/form.py
+++ account/form.py
@@ -8,8 +8,40 @@ class ShopUserCreationForm(UserCreationForm):
         model = ShopUser
         fields = ('phone', 'address', 'first_name', 'last_name', 'is_active', 'is_superuser', 'date_joined')
 
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if self.instance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('already exited')
+            else:
+                if ShopUser.objects.filter(phone=phone).exists():
+                    raise forms.ValidationError('already exited')
+        if ShopUser.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('already exited')
+        if not phone.isdigit():
+            raise forms.ValidationError('phone must be started with 09')
+        if len(phone) != 11:
+            raise forms.ValidationError('phone must have 11 digits')
+        return phone
+
 
 class ShopUserChangeForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = ShopUser
         fields = ('phone', 'address', 'first_name', 'last_name', 'is_active', 'is_superuser', 'date_joined')
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if self.instance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('already exited')
+            else:
+                if ShopUser.objects.filter(phone=phone).exists():
+                    raise forms.ValidationError('already exited')
+        if ShopUser.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('already exited')
+        if not phone.isdigit():
+            raise forms.ValidationError('phone must be started with 09')
+        if len(phone) != 11:
+            raise forms.ValidationError('phone must have 11 digits')
+        return phone
