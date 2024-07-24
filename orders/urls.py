167572ddd60930c2e566a9f2999c9310b22bebf1
nell2008 <ashkan.spg@gmail.com>
2024-07-24 17:55:00 -0500

    1

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('verify-phone', views.verify_phone,name='verify_phone'),
-    path('verify-code', views.verify_code, name='verify_code')
+    path('verify-phone', views.verify_phone, name='verify_phone'),
+    path('verify-code', views.verify_code, name='verify_code'),
+    path('create-order', views.order_create, name='create_order')
 ]
