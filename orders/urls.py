765575f0eb4ef9edd9591b829819f3fde9cb6bd7
nell2008 <ashkan.spg@gmail.com>
2024-08-12 13:38:52 -0500

    1

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,7 @@ app_name = 'orders'
 urlpatterns = [
     path('verify-phone', views.verify_phone, name='verify_phone'),
     path('verify-code', views.verify_code, name='verify_code'),
-    path('create-order', views.order_create, name='create_order')
+    path('create-order', views.order_create, name='create_order'),
+    path('request/', views.send_request, name='request'),
+    path('verify/', views.verify, name='verify'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -64,9 +64,82 @@ def order_create(request):
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
                                          quantity=item['quantity']
                                          , weight=item['weight'])
-            cart.clear()
+            #cart.clear()
             return redirect('shop:products_list')
 
     else:
         form = OrderCreateForm()
     return render(request, 'order/order_create.html', {'form': form, 'cart': cart})
+
+
+from django.conf import settings
+import requests
+import json
+
+# ? sandbox merchant
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+
+
+# Important: need to edit for realy server.
+CallbackURL = 'http://127.0.0.1:7000/verify/'
+
+
+def send_request(request):
+    cart = Cart(request)
+    description = ''
+    for item in cart:
+        description += str(item.product.name)
+    data = {
+        "MerchantID": settings.MERCHANT,
+        "Amount": cart.get_final_price(),
+        "Description": description,
+        "Phone": request.user.phone,
+        "CallbackURL": CallbackURL,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+    try:
+        response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+        if response.status_code == 200:
+            response = response.json()
+            if response['Status'] == 100:
+                cart.clear()
+                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+                        'authority': response['Authority']}
+            else:
+                return {'status': False, 'code': str(response['Status'])}
+        return response
+
+    except requests.exceptions.Timeout:
+        return {'status': False, 'code': 'timeout'}
+    except requests.exceptions.ConnectionError:
+        return {'status': False, 'code': 'connection error'}
+
+
+def verify(authority):
+    data = {
+        "MerchantID": settings.MERCHANT,
+        #"Amount": amount,
+        "Authority": authority,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+    response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+
+    if response.status_code == 200:
+        response = response.json()
+        if response['Status'] == 100:
+            return {'status': True, 'RefID': response['RefID']}
+        else:
+            return {'status': False, 'code': str(response['Status'])}
+    return response
