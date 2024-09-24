1a2aec553e38dd8988d22658cec7b106cea79f9d
nell2008 <ashkan.spg@gmail.com>
2024-09-23 19:03:40 -0500

    1

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'orders'
+app_name = 'api'
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view(), name='products_list_api'),
     path('products/<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('users/', views.UserListAPIView.as_view(), name='user_list_api')
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from shop.models import Product
-from .serializers import ProductSerializer
+from .serializers import *
 from rest_framework import generics
+from rest_framework import views
+from account.models import ShopUser
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import BasicAuthentication
 
 
 # Create your views here.
@@ -15,3 +20,13 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+
+
+class UserListAPIView(views.APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        users = ShopUser.objects.all()
+        serializer = ShopUserSerializer(users, many=True)
+        return Response(serializer.data)
